c430c3a6ddbf1eacac973c7b7af97936e87f79e9
krizhnaa <krizhnaprem@gmail.com>
2024-01-29 22:00:31 +0530

    Collecting blog data through API

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    blogs = response.json()
+    return render_template("index.html", blogs=blogs)
 
 if __name__ == "__main__":
     app.run(debug=True)
