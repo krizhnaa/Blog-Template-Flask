25ecb7b45de165f0950f0dabe9716873146b99c0
krizhnaa <krizhnaprem@gmail.com>
2024-02-02 10:28:46 +0530

    minor changes

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def home():
     # blogs = response.json()
     return render_template("index.html")
 
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 # @app.route('/blog/<int:id>')
 # def get_blog(id):
 #     response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
