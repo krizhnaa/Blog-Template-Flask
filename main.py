686cf0a11819817e7df5b18888c34dec02b1d656
krizhnaa <krizhnaprem@gmail.com>
2024-02-02 09:29:02 +0530

    Creating an upgraded version of the Blog with Bootstrap

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    blogs = response.json()
-    return render_template("index.html", blogs=blogs)
+    # response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    # blogs = response.json()
+    return render_template("index.html")
 
-@app.route('/blog/<int:id>')
-def get_blog(id):
-    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    blogs = response.json()
-    return render_template("post.html", blog_id=id, blogs=blogs)
+# @app.route('/blog/<int:id>')
+# def get_blog(id):
+#     response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+#     blogs = response.json()
+#     return render_template("post.html", blog_id=id, blogs=blogs)
 
 if __name__ == "__main__":
     app.run(debug=True)
